1314aed86079d8c049df0df977af4ac7e216b1c7
RedRain715 <redrain715@gmail.com>
2025-02-02 16:53:47 -0600

    "Update"

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Hello!');
+const savedPlayerData = localStorage.getItem('player');
+const player = savedPlayerData ? JSON.parse(savedPlayerData): {
+    energy: 0,
+    energyPerSec: 1,
+}
+const savedGameData = localStorage.getItem('game');
+const game = savedGameData ? JSON.parse(savedGameData): {
+    delay: 650,//delay for energyPerSec
+}
+function addEnergy() {
+    player.energy += player.energyPerSec;
+    updateUI();
+    saveGame();
+}
+function updateUI() {
+    document.title = 'Energy: ' + player.energy //Change title
+    document.getElementById('energy').innerText = player.energy;
+}
+function saveGame() {
+    localStorage.setItem('player', JSON.stringify(player));
+    localStorage.setItem('game', JSON.stringify(game));
+}
+console.log(localStorage.getItem('player')); // Check stored data
+updateUI();
+setInterval(() => {
+    addEnergy();
+}, game.delay);
+
